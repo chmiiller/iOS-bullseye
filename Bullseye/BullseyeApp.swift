19d4a20e6ee1a09c3d1da32e46029905fe3f2aa7
Carlos Zinato <carlos.zinato@tomtom.com>
2024-01-26 18:10:36 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Carlos Zinato on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
