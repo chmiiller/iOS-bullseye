c2068ec2824e321cadab4f67a0134f20f3a96558
Carlos Zinato <carlosh.lz@hotmail.com>
2024-02-25 19:33:20 +0100

    adds tests and score functionality

--- Bullseye/Models/Game.swift
+++ Bullseye/Models/Game.swift
@@ -8,11 +8,12 @@
 import Foundation
 
 struct Game {
-    var target: Int = 37
+    var target: Int = Int.random(in: 1...100)
     var score: Int = 0
     var round: Int = 1
     
     func points(sliderValue: Int) -> Int {
-        return 999
+        let diff: Int = abs(target - sliderValue)
+        return 100 - diff
     }
 }
