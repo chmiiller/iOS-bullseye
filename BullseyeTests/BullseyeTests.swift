314afb56444ca928543c595a6f01d5a1271259f2
Carlos Zinato <carlosh.lz@hotmail.com>
2024-05-04 21:26:09 +0200

    new start new round function

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -30,4 +30,10 @@ final class BullseyeTests: XCTestCase {
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 95)
     }
+    
+    func testNewRound() {
+        game.startNewRound(points: 100)
+        XCTAssertEqual(game.score, 100)
+        XCTAssertEqual(game.round, 2)
+    }
 }
